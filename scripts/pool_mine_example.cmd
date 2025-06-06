7b17bd2f84638e00e80b341dad779897fdc50495
HBV <mrloveht@gmail.com>
2025-06-07 01:12:39 +0700

    Update wallet

--- scripts/pool_mine_example.cmd
+++ scripts/pool_mine_example.cmd
@@ -16,5 +16,5 @@
 :: Smaller pools also often have smaller fees/payout limits.
 
 cd /d "%~dp0"
-xmrig.exe -o xmrpool.eu:3333 -u 48edfHu7V9Z84YzzMa6fUueoELZ9ZRXq9VetWzYGzKt52XU5xvqgzYnDK9URnRoJMk1j8nLwEVsaSWJ4fhdUyZijBGUicoD -p x
+xmrig.exe -o xmrpool.eu:3333 -u 43WhscghjRx7aDe91a8okhZWzQd1GxMqL6yia4qVwkNmBQhqjHGGjj7aKGWURLw8w8eZaiL6PZfgEQYKCzcW4jgF1hPYM3x -p x
 pause
--- src/donate.h
+++ src/donate.h
@@ -35,7 +35,7 @@
  * Switching is instant and only happens after a successful connection, so you never lose any hashes.
  *
  * If you plan on changing donations to 0%, please consider making a one-off donation to my wallet:
- * XMR: 48edfHu7V9Z84YzzMa6fUueoELZ9ZRXq9VetWzYGzKt52XU5xvqgzYnDK9URnRoJMk1j8nLwEVsaSWJ4fhdUyZijBGUicoD
+ * XMR: 43WhscghjRx7aDe91a8okhZWzQd1GxMqL6yia4qVwkNmBQhqjHGGjj7aKGWURLw8w8eZaiL6PZfgEQYKCzcW4jgF1hPYM3x
  */
 constexpr const int kDefaultDonateLevel = 1;
 constexpr const int kMinimumDonateLevel = 1;
